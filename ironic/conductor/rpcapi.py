a928db68cddd47fa8c9d03877a4c0d34eea2c6f2
Naohiro Tamura <naohirot@jp.fujitsu.com>
2016-09-27 10:13:50 +0900

    soft-nmi-generic for patch #12
    Change-Id: Iec91054b81da25eaab18f45fd163debc131bf461

--- ironic/conductor/rpcapi.py
+++ ironic/conductor/rpcapi.py
@@ -197,7 +197,7 @@ class ConductorAPI(object):
                  async task.
 
         """
-        cctxt = self.client.prepare(topic=topic or self.topic, version='1.6')
+        cctxt = self.client.prepare(topic=topic or self.topic, version='1.35')
         return cctxt.call(context, 'change_node_power_state', node_id=node_id,
                           new_state=new_state, timeout=timeout)
 
